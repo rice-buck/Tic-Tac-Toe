df96acae06bd6dd8e230390f808c189bb4e988db
rice-buck <rhysobuckeye@outlook.com>
2025-11-04 11:10:18 -0800

    test mac

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,20 @@
-//test
+#include <iostream> 
+#include <vector> 
+#include "functions.h"
+using namespace std;
+
+void displayGrid(){
+    std::vector<std::vector<char>>charMatrix(3,std::vector<char>(3,' '));
+
+    charMatrix[0][0] = 'A';
+    charMatrix[1][1] = 'B';
+    charMatrix[2][2] = 'C';
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << charMatrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+}
+
+}
--- functions.h
+++ functions.h
@@ -1,2 +1,5 @@
+#include <vector>
 //3x3 char vector
 //outer vector has 3 rows, each inner vector has 3 columns
+void displayGrid();
+
